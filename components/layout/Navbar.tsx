1c5f981f85dbb736cfca6b2ad51fdfbe6ff3baef
Mezdoud Ala Eddine <mezdoud.ala.e@gmail.com>
2025-12-10 22:48:06 +0100

    navbar container

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -15,7 +15,7 @@ export default function Navbar() {
 
     return (
         <nav className="bg-white shadow-sm fixed top-0 left-0 right-0 z-50">
-            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+            <div className="max-w-7xl mx-auto container">
                 <div className="flex justify-between h-18">
                     {/* Logo */}
                     <div className="flex-shrink-0 flex items-center">
