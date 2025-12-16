35888b6bb6dcebc7311322d99e4dbc3833b5e535
Meriem Ouadfel <165055386+Meryy0@users.noreply.github.com>
2025-12-16 21:40:23 +0100

    fixing layout

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -1,6 +1,5 @@
 import { Poppins, Inter } from "next/font/google";
-import Navbar from "@/components/layout/Navbar";
-import Footer from "@/components/layout/Footer";
+import ConditionalLayout from "@/components/layout/ConditionalLayout";
 import "../globals.css";
 
 const poppins = Poppins({
@@ -25,9 +24,9 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${poppins.variable} ${inter.variable}`} suppressHydrationWarning>
       <body className="font-inter">
-        <Navbar />
-        {children}
-        <Footer />
+        <ConditionalLayout>
+          {children}
+        </ConditionalLayout>
       </body>
     </html>
   );
